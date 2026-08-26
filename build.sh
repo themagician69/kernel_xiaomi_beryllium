a205b47da6d9e38cfa7669957b093ad5bfbbb535
themagician69 <sacrovano@gmail.com>
2026-08-26 21:20:19 +0700

    Update KSU

--- build.sh
+++ build.sh
@@ -9,7 +9,7 @@ echo -e "*****************************"
 export LLVM=1
 
 # KernelSU-Next
-curl -LSs "https://raw.githubusercontent.com/rifsxd/KernelSU-Next/next/kernel/setup.sh" | bash -s v1.0.2-R14
+curl -LSs "https://raw.githubusercontent.com/rifsxd/KernelSU-Next/next/kernel/setup.sh" | bash -s v1.1.1
 
 # Clang
 echo "Using Prelude-Clang"
